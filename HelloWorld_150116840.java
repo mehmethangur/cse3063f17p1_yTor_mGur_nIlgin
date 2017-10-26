f48a312725ef525472384174f6603b33da850c53
mehmethangur <mehmethangur@marun.edu.tr>
2017-10-26 13:48:07 +0300

    Update and rename HelloWorld.java to HelloWorld_150116840.java

--- HelloWorld_150116840.java
+++ HelloWorld_150116840.java
@@ -1,6 +1,6 @@
 //Mehmethan GÜR
 //150116840
-public class HelloWorld {
+public class HelloWorld_150116840 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
